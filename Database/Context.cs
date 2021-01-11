cb43b04a3c97e193b41051aa66bfb80ad8fcec82
inaba <software@panduro.guru>
2021-01-11 02:12:53 +0100

    ?

--- Database/Context.cs
+++ Database/Context.cs
@@ -1,10 +1,10 @@
 using System.Collections.Generic;
 using Microsoft.EntityFrameworkCore;
 
-namespace Database.Context 
+namespace Database.Context
 {
-    public class MovieContext : DbContext 
-    {        
-        public override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) => optionsBuilder.UseNpgsql("Host=localhost;Database=Streaminator;Username=localhost;Password=1234");
+    public class MovieContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) => optionsBuilder.UseNpgsql("Host=localhost;Database=Streaminator;Username=localhost;Password=1234");
     }
 }
